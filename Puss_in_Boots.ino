ba4f5cc6c2aceec327251c55c3b608e646122609
schowdhury2929 <114033417+schowdhury2929@users.noreply.github.com>
2023-11-06 17:06:40 -0600

    Update Puss_in_Boots.ino

--- Puss_in_Boots.ino
+++ Puss_in_Boots.ino
@@ -1,32 +1,39 @@
+#include <Servo.h>
+
+Servo kitty;
+//Servo death;
+//Servo weapon;
+
+const int buttonPin = 2;
+
 void setup() {
-  /* 
-  servo for Kitty
-  servo for Death
-  servo for Sword/Hat
-  */
-  
+  kitty.attach(4);
+  kitty.write(0);
+  //death.attach();
+  //weapon.attach();
+  pinMode(buttonPin, INPUT);
 
   bool fear = true;
   bool prepared = false;
   bool loss = false;
 }
 
-void loop() {
-  /* 
-  if wish on base
-    Kitty faces Puss
-
-    if Puss and Kitty touch paws & fear == true
-      team();
+void loop() { 
+  if (digitalRead(buttonPin) == HIGH) {
+    kitty.write(90);
+  }
+  /*
+  if Puss and Kitty touch paws & fear == true
+    team();
     
-    if Puss is on base & fear == false
-      prepared();
+  if Puss is on base & fear == false
+    prepared();
         
-    if Puss near Death & prepared == true & loss == false
-        loss();
+  if Puss near Death & prepared == true & loss == false
+    loss();
 
-    if sword and scythe cross
-      win();
+  if sword and scythe cross
+    win();
   */
 }
 
