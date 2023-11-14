dbed923bd88071dfd1a4e085df4df224faad2ed9
schowdhury2929 <114033417+schowdhury2929@users.noreply.github.com>
2023-11-13 18:05:19 -0600

    Update Puss_in_Boots.ino

--- Puss_in_Boots.ino
+++ Puss_in_Boots.ino
@@ -1,69 +1,94 @@
 #include <Servo.h>
 
 Servo kitty;
-//Servo death;
-//Servo weapon;
+Servo death;
+Servo weapon;
 
-const int buttonPin = 2;
+const int wish = 2;
+const int paws = 3;
+const int boots = 4;
+const int fight = 5;
 
+const int fire = 13;
+
+bool fear = true;
+bool prepared = false;
+bool lost = false;
+
+//sets up servos and gets them into position
 void setup() {
-  kitty.attach(4);
-  kitty.write(0);
-  //death.attach();
-  //weapon.attach();
-  pinMode(buttonPin, INPUT);
+  kitty.attach(8);
+  kitty.write(180);
+  death.attach(9);
+  death.write(0);
+  weapon.attach(10);
+  weapon.write(0);
 
-  bool fear = true;
-  bool prepared = false;
-  bool loss = false;
+  pinMode(wish, INPUT);
+  pinMode(paws, INPUT);
+  pinMode(boots, INPUT);
+  pinMode(fight, INPUT);
+
+  pinMode(fire, OUTPUT);
+  digitalWrite(fire, LOW);
 }
 
-void loop() { 
-  if (digitalRead(buttonPin) == HIGH) {
-    kitty.write(90);
-  }
-  /*
-  if Puss and Kitty touch paws & fear == true
-    team();
-    
-  if Puss is on base & fear == false
-    prepared();
-        
-  if Puss near Death & prepared == true & loss == false
-    loss();
+//calls for the rest of the functions when they are activated in the enclosure
+void loop() {
 
+  //makes Kitty block Puss's way to the arena after the wish is removed from him
+  if (digitalRead(wish) == HIGH & fear == true) {
+    kitty.write(0);
+  }
+
+  if (digitalRead(paws) == HIGH & fear == true) {
+    team();
+  }
+
+  if (digitalRead(boots) == HIGH & fear == false) {
+    light();
+  }
+
+  if (digitalRead(fight) == HIGH & prepared == true & lost == false) {
+    loss();
+  }
+
+  /*          
   if sword and scythe cross
     win();
   */
 }
 
+//gets Kitty out of Puss's way when they touch
 void team() {
-  /*
-  Kitty turns around
+  kitty.write(90);
   fear = false;
-  */
 }
 
-void prepared() {
-  /*
-  fire up
-  fight = true
-  */
+//lights up the arena and allows Puss and Death to fight
+void light() {
+  digitalWrite(fire, HIGH);
+  prepared = true;
 }
 
+//the first fight between Puss and Death, starts once Puss is near Death
 void loss() {
-  /*
-  30 degrees forward and back
-  loss = true;
-  switch Death to scythe
-  weapon revealed
-  */
+  death.write(90);
+  death.write(60);
+  death.write(120);
+  death.write(60);
+  death.write(120);
+  death.write(90);
+  lost = true;
+  weapon.write(90);
 }
 
+//the second fight between Puss and Death, starts once Puss approaches Death with his weapon
 void win() {
-  /*
-  30 degrees forward and back
-  switch Death to lose side
-  fire down
-  */
+  death.write(60);
+  death.write(120);
+  death.write(60);
+  death.write(120);
+  death.write(180);
+  digitalWrite(fire, LOW);
 }
